96ceaf4c1346d01c0fd879129c802bb052dbf1a2
jeffyoung20 <jeffyoung20@hotmail.com>
2023-02-28 22:32:55 -0500

    Added DTOs

--- spring-restapi-jpa/src/main/java/com/jeffyoung20/dataserver/repos/AddressRepo.java
+++ spring-restapi-jpa/src/main/java/com/jeffyoung20/dataserver/repos/AddressRepo.java
@@ -2,7 +2,7 @@ package com.jeffyoung20.dataserver.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.jeffyoung20.dataserver.models.Address;
+import com.jeffyoung20.dataserver.models.data.Address;
 
 public interface AddressRepo extends JpaRepository<Address,Long> {
 
--- spring-restapi-jpa/src/main/java/com/jeffyoung20/dataserver/repos/PersonRepo.java
+++ spring-restapi-jpa/src/main/java/com/jeffyoung20/dataserver/repos/PersonRepo.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.jeffyoung20.dataserver.models.Person;
+import com.jeffyoung20.dataserver.models.data.Person;
 
 import jakarta.persistence.Entity;
 import lombok.Data;
