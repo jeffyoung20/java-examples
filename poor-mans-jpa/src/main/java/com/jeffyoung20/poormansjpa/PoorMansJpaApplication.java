2e72f2c111ebe240bac3e7da0ec426448e4ee613
jeffyoung20 <jeffyoung20@hotmail.com>
2023-03-15 21:07:21 -0400

    Initial project creation

---
+++ poor-mans-jpa/src/main/java/com/jeffyoung20/poormansjpa/PoorMansJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jeffyoung20.poormansjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoorMansJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoorMansJpaApplication.class, args);
+	}
+
+}
