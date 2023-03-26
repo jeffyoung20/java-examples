c736bde9996f32f2e79d5cbe70fe028ad1bfe1cd
jeffyoung20 <jeffyoung20@hotmail.com>
2023-03-26 19:56:19 -0400

    Added h2 database

--- poor-mans-jpa/poor-mans-jpa-test-app/src/main/java/com/jeffyoung20/testapp/controllers/DatabaseController.java
+++ poor-mans-jpa/poor-mans-jpa-test-app/src/main/java/com/jeffyoung20/testapp/controllers/DatabaseController.java
@@ -1,10 +1,7 @@
 package com.jeffyoung20.testapp.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +20,7 @@ import com.jeffyoung20.testapp.repos.RepoPerson;
 
 
 @RestController
+@RequestMapping("/api")
 public class DatabaseController {
 	@Autowired
 	private RepoPerson repoPerson;
